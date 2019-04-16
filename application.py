acb8a5b96c2eeacd72b97f19d4eeb4ef7027374a
ws0h3ll <cswilson326@live.com>
2019-04-16 08:46:39 +0100

    first commit

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+db = SQLAlchemy()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('settings.py')
+    db.init_app(app)
+    migrate = Migrate(app,db)
+
+    from counter.views import counter_app
+    app.register_blueprint(counter_app)
+    return app
---
+++ counter/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from counter.models import Counter
+from application import db
+counter_app = Blueprint('counter_app',__name__)
+
+@counter_app.route('/')
+def init():
+    counter = Counter.query.first()
+    if not counter:
+        counter = Counter(1)
+        db.session.add(counter)
+        db.session.commit()
+    else:
+        counter.count += 1
+        db.session.commit()
+    return '<h1>Counter : ' + str(counter.count) + '</h1>'
